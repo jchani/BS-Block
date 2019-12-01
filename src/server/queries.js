1880ef35c8f3179e64e0d987af351f90706afc4b
jchani <justinchan11@hotmail.com>
2019-11-30 20:15:48 -0800

    Hitting RDS database + moved credentials to env file

--- src/server/queries.js
+++ src/server/queries.js
@@ -2,13 +2,15 @@
 
 //TODO: move these credentials to env file
 const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'twiliodb',
-  password: 'password',
-  port: 5432,
-})
+console.log(process.env);
+// const pool = new Pool({
+//   user: 'jchani',
+//   host: 'database-1.crelazp3gyca.us-west-2.rds.amazonaws.com',
+//   database: 'postgres_1',
+//   password: 'enknckj36n2dkac;3kd',
+//   port: 5432,
+// })
+const pool = new Pool() //uses values defined in env file
 
 /**
  *  --- AUTHENTICATION METHODS ---
@@ -19,7 +21,7 @@ const pool = new Pool({
  */
 const storeAccessToken = (phone, token) => {
   return new Promise((resolve, reject) => {
-    pool.query('SELECT token FROM users WHERE phone=$1;', [phone])
+    pool.query('SELECT token FROM bsblock_users WHERE phone=$1;', [phone])
     .then(results => {
       if(results.rows.length > 0) { //overwrite existing token
         return resolve(updateToken(phone, token));
@@ -27,7 +29,7 @@ const storeAccessToken = (phone, token) => {
         return resolve(createUser(phone, token));
       }
     })
-    .catch(error => logAndReject(reject));
+    .catch(error => logAndReject(error, reject));
   });
 }
 
@@ -36,9 +38,9 @@ const storeAccessToken = (phone, token) => {
  */
 const validatePhoneAndAccessToken = (phone, token) => {
   return new Promise((resolve, reject) => {
-    pool.query(`SELECT token FROM users WHERE phone=$1 AND token=$2`, [phone, token])
+    pool.query(`SELECT token FROM bsblock_users WHERE phone=$1 AND token=$2`, [phone, token])
     .then(results => resolve(results.rows.length > 0 ? true : false))
-    .catch(error => logAndReject(reject))
+    .catch(error => logAndReject(error, reject))
   });
 }
 
@@ -47,9 +49,9 @@ const validatePhoneAndAccessToken = (phone, token) => {
  */
 const incrementCalls = (phone) => { 
   return new Promise((resolve, reject) => {
-    pool.query('UPDATE users SET calls = calls+1 WHERE phone = $1', [phone])
+    pool.query('UPDATE bsblock_users SET calls = calls+1 WHERE phone = $1', [phone])
     .then(x => resolve(x))
-    .catch(error => logAndReject(reject));
+    .catch(error => logAndReject(error, reject));
   });
 }
 
@@ -58,20 +60,20 @@ const incrementCalls = (phone) => {
  */
 const createUser = (phone, token) => {
   return new Promise((resolve, reject) => {
-    pool.query(`INSERT INTO users(phone,calls,paid,token) VALUES ($1, 0, 0, $2);`, [phone ,token])
+    pool.query(`INSERT INTO bsblock_users(phone,calls,paid,token) VALUES ($1, 0, 0, $2);`, [phone ,token])
     .then(x => {
       return Promise.resolve();
     })
     .then(x => resolve(x))
-    .catch(error => logAndReject(reject));
+    .catch(error => logAndReject(error, reject));
   });
 }
 
 const updateToken = (phone, token) => {
   return new Promise((resolve, reject) => {
-    pool.query(`UPDATE users SET token=$2 WHERE phone=$1;`, [phone, token])
+    pool.query(`UPDATE bsblock_users SET token=$2 WHERE phone=$1;`, [phone, token])
     .then(x => resolve(x))
-    .catch(error => logAndReject(reject));
+    .catch(error => logAndReject(error, reject));
   });
 }
 
@@ -80,24 +82,24 @@ const updateToken = (phone, token) => {
  */
 const getCallDataByPhone = (phone) => {
   return new Promise((resolve, reject) => {
-    pool.query(`SELECT phone, calls FROM users WHERE phone = $1`, [phone])
+    pool.query(`SELECT phone, calls FROM bsblock_users WHERE phone = $1`, [phone])
     .then(results => resolve(results.rows))
-    .catch(error => logAndReject(error));
+    .catch(error => logAndReject(error, reject));
   });
 }
 
 const getAllUsersCallData = (request, response) => {
   return new Promise((resolve, reject) => {
-    pool.query('SELECT SUM(calls) AS calls, SUM(paid) AS paid FROM users')
+    pool.query('SELECT SUM(calls) AS calls, SUM(paid) AS paid FROM bsblock_users')
     .then(results => resolve(response.status(200).json(results.rows)))
-    .catch(error => logAndReject(error));
+    .catch(error => logAndReject(error, reject));
   });
 }
 
 /**
  * --- UTILITY METHODS ---
  */
-function logAndReject(reject) {
+function logAndReject(error, reject) {
   console.log(error);
   reject(error);
 }
@@ -109,7 +111,7 @@ function logAndReject(reject) {
 
 const updateUser = (request, response) => {
   const { phone, calls } = request.body
-  pool.query('UPDATE users SET calls=$2 WHERE phone = $1',
+  pool.query('UPDATE bsblock_users SET calls=$2 WHERE phone = $1',
     [phone, calls],
     (error, results) => {
     if(error) console.log(error);
@@ -119,7 +121,7 @@ const updateUser = (request, response) => {
 
 const deleteUser = (request, response) => {
   const phone = parseInt(request.params.phone)
-  pool.query('DELETE FROM users WHERE phone = $1',
+  pool.query('DELETE FROM bsblock_users WHERE phone = $1',
     [phone],
     (error, results) => {
     if(error) console.log(error);
